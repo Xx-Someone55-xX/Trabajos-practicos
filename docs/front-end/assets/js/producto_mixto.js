6f195f6e513ab80301f339b95eb3e869d86d03fe
Agustín Giusso <agustingiusso@gmail.com>
2025-06-23 05:19:53 -0300

    TP grupal Álgebra casi terminado

--- docs/front-end/assets/js/producto_mixto.js
+++ docs/front-end/assets/js/producto_mixto.js
@@ -24,7 +24,7 @@ function init() {
         0.1, 
         1000
     );
-    camera.position.set(5, 5, 5);
+    camera.position.set(5, 5, 10);
     camera.lookAt(0, 0, 0);
     
     // 3. Renderizador con antialiasing
